e56eb0f9069de168f3e7de3dd69ee1ef3f9b02fa
Mohammad Haghighat <mhaghighat@rayana.ir>
2018-11-26 11:55:31 +0330

    Add project files.

---
+++ Bekhar/Models/Kala.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bekhar.Models
+{
+    public class Kala
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long Price { get; set; }
+    }
+}
---
+++ Bekhar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bekhar.Startup))]
+namespace Bekhar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
