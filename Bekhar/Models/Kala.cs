d3dc4888aec5058539b3f9a8f2bb40c82027a97a
Mohammad Haghighat <mhaghighat@rayana.ir>
2018-12-01 11:18:49 +0330

    اضافه ولیدیشن ها

--- Bekhar/Models/Kala.cs
+++ Bekhar/Models/Kala.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,22 @@ namespace Bekhar.Models
     public class Kala
     {
         public string Id { get; set; }
+
+        [Display(Name = "عنوان کالا")]
+        [Required(ErrorMessage = "وارد کردن عنوان ضروری است.")]
         public string Name { get; set; }
+
+        [Display(Name = "توضیحات کالا")]
         public string Description { get; set; }
+
+        [Display(Name = "قیمت کالا")]
         public long? Price { get; set; }
+        [Display(Name = "موبایل")]
         public string Mobile { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "وارد کردن ایمیل ضروری است.")]
+        [EmailAddress(ErrorMessage = "ایمیل نامعتبر")]
         public string Email { get; set; }
         public DateTime CreationTime { get; set; }
 
