05a7705d032868244b882a9e3303c752415f7346
Mohammad Haghighat <mhaghighat@rayana.ir>
2019-01-31 22:23:37 +0330

    بالا آمدن کلاستر الاستیک

--- Bekhar/Elastic/EsClient.cs
+++ Bekhar/Elastic/EsClient.cs
@@ -15,7 +15,7 @@ namespace Bekhar.Elastic
             {
                 //var settings = new ConnectionSettings(new Uri("http://localhost:9200")).DefaultIndex("bekhar").DisableDirectStreaming();
 
-                var settings = new ConnectionSettings(new Uri("https://elastic:Hq7ntRjOu8BQzQhhVB2ZSpws@58ef425b3817474986de023092358b39.eu-central-1.aws.cloud.es.io:9243")).DefaultIndex("bekhar").DisableDirectStreaming();
+                var settings = new ConnectionSettings(new Uri("https://elastic:1IeomFYO4yVyqzlrO95SpXFy@a8769f2176114de3ac5fb6371bd61b21.eu-central-1.aws.cloud.es.io:9243")).DefaultIndex("bekhar").DisableDirectStreaming();
                 innerClient = new ElasticClient(settings);
             }
 
