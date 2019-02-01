3501f407afe8fb09d6e6dd26529a8d96084eaa95
Qolenj <salehv@gmail.com>
2019-02-01 19:09:33 +0330

    خریدز

--- Bekhar/Models/Kharid.cs
+++ Bekhar/Models/Kharid.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,18 +9,24 @@ namespace Bekhar.Models
 {
     public class Kharid : ElasticDocument
     {
+        [Display(Name = "زمان معامله")]
         public DateTime CreationTime { get; set; }
 
+        [Display(Name="#")]
         public string KalaId { get; set; }
 
+        [Display(Name = "خریدار")]
         public string KharidarUsername { get; set; }
+        [Display(Name = "فروشنده")]
         public string ForoshandeUsername { get; set; }
 
+        [Display(Name = "وضعیت معامله")]
         public KharidState State { get; set; }
 
         [JsonIgnore]
         public bool canBeApproved;
 
+        [Display(Name = "نوع معامله")]
         public ModelType DataType { get; set; } = ModelType.Kharid;
     }
 
