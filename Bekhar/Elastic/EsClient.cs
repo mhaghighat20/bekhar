5ef158d45404864d6ec9fde99f40d8ee6ed1a76c
Mohammad Haghighat <mhaghighat@rayana.ir>
2019-02-01 13:42:32 +0330

    حل مشکل کش کانکشن های الاستیک

--- Bekhar/Elastic/EsClient.cs
+++ Bekhar/Elastic/EsClient.cs
@@ -8,18 +8,18 @@ namespace Bekhar.Elastic
 {
     internal class EsClient
     {
-        private static ElasticClient innerClient;
+        private static Dictionary<string, ElasticClient> innerClients = new Dictionary<string, ElasticClient>();
         public static ElasticClient GetInstance(string indexName)
         {
-            if (innerClient == null)
+            if (!innerClients.ContainsKey(indexName))
             {
                 //var settings = new ConnectionSettings(new Uri("http://localhost:9200")).DefaultIndex("bekhar").DisableDirectStreaming();
 
                 var settings = new ConnectionSettings(new Uri("https://elastic:1IeomFYO4yVyqzlrO95SpXFy@a8769f2176114de3ac5fb6371bd61b21.eu-central-1.aws.cloud.es.io:9243")).DefaultIndex(indexName).DisableDirectStreaming();
-                innerClient = new ElasticClient(settings);
+                innerClients.Add(indexName, new ElasticClient(settings));
             }
 
-            return innerClient;
+            return innerClients[indexName];
         }
 
     }
